3b5791a964d285305ea64e5eed57f0e529e1bdac
Catalina <catarusso07@hotmail.com>
2022-04-20 16:30:54 -0300

    Primeros  cambios del proyecto

---
+++ src/women.js
@@ -0,0 +1,12 @@
+class products {
+    constructor(id, name, price, img) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.img = img;
+    }
+
+}
+
+const prod = [];
+
