2fd06478877085c9326114498b892c7b8f389a7e
Catalina <catarusso07@hotmail.com>
2022-04-20 16:50:53 -0300

    class y productos agregados

--- src/women.js
+++ src/women.js
@@ -10,3 +10,8 @@ class products {
 
 const prod = [];
 
+const card1 = new products ( 1, "Colorblock Drop-Sleeve", 3550,"../Assets/Multimedia/fotos_women/Colorblock Drop-Sleeve Shirt 1.webp");
+const card2 = new products ( 2, "Hot Wheels Graphic Cami", 2000,"../Assets/Multimedia/fotos_women/Hot Wheels Graphic Cami 1.jpg");
+const card3 = new products ( 3, "Pointelle Knit Tank Top", 1500,"../Assets/Multimedia/fotos_women/Pointelle Knit Tank Top 1.webp");
+const card4 = new products ( 4, "Patchwork Bandana Crop Top", 2000,"../Assets/Multimedia/fotos_women/Patchwork Bandana Crop Top 1.jpg" )
+prod.push(card1, card2, card3, card4);
