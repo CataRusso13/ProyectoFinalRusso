aa91b4850c8e7806e84cf853b6ae853f9d7b96fb
Catalina <catarusso07@hotmail.com>
2022-04-23 20:56:04 -0300

    .

--- src/women.js
+++ src/women.js
@@ -1,16 +1,15 @@
 let itemRow = document.querySelector("#row");
 class products {
     constructor(id, name, price, img) {
-        this.id = id;
-        this.name = name;
-        this.price = price;
-        this.img = img;
+        this.id = id,
+        this.name = name,
+        this.price = price,
+        this.img = img
     }
-
 }
-
 const prods = [];
-let bag1 = JSON.parse(localStorage.getItem("BagCloud")) || [];
+const bag = JSON.parse(localStorage.getItem("prodsBag")) || [];
+render();
 
 const card1 = new products ( 1, "Colorblock Drop-Sleeve", 3550,"../Assets/Multimedia/fotos_women/Colorblock Drop-Sleeve Shirt 1.webp");
 const card2 = new products ( 2, "Hot Wheels Graphic Cami", 2000,"../Assets/Multimedia/fotos_women/Hot Wheels Graphic Cami 1.jpg");
@@ -19,18 +18,30 @@ const card4 = new products ( 4, "Patchwork Bandana Crop Top", 2000,"../Assets/Mu
 prods.push(card1, card2, card3, card4);
 
 for (const item of prods) {
-let items = document.createElement("div");
-    items.className = " women-card col-12 col-sm-12 col-md-6 col-lg-3"
+    let items = document.createElement("div");
+    items.className = " women-card col-12 col-sm-12 col-md-6 col-lg-3";
     items.innerHTML = ` <img src="${item.img}"alt="foto Drop-Sleeve"> </img>
-                        <h2>${item.name}</h2>
-                        <p>$${item.price}</p>
-                        <a id=${item.id} class=" btn  addToBag ">Add to bag</a>`
+    <h2>${item.name}</h2>
+    <p>$${item.price}</p>
+    <a id=${item.id} class=" btn  addToBag ">Add to bag</a>`
     itemRow.appendChild(items)
-    let addToBagButton = document.getElementById(`${item.id}`);
-    addToBagButton.addEventListener('click', bagButton);
+    let btnAddToBag = document.getElementById(`${item.id}`).addEventListener("click", () => bagButton(item));
+}
+
+function render () {
+    const callModal = document.querySelector("#modalB");
+    bag.forEach(item => {
+        let bagItemContent = document.createElement("div");
+        bagItemContent.innerHTML = `
+        <p>Name:${item.name} Price: $${item.price}</p>  
+        `
+    })
+    callModal.append(bagItemContent);  
 }
 
 function bagButton (item) {
-    bag1.push(item);
-    localStorage.setItem("BagCloud", JSON.stringify(item));
+    bag.push(item);
+    render();
+    localStorage.setItem("prodsBag", JSON.stringify(item));
 }
+
