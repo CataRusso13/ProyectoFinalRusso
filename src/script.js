09643ea8655b0b291a0372fd85b1954446b15102
Catalina <catarusso07@hotmail.com>
2022-04-23 21:06:03 -0300

    .

--- src/script.js
+++ src/script.js
@@ -2,19 +2,19 @@ let itemRow = document.querySelector("#row");
 class products {
     constructor(id, name, price, img) {
         this.id = id,
-        this.name = name,
-        this.price = price,
-        this.img = img
+            this.name = name,
+            this.price = price,
+            this.img = img
     }
 }
 const prods = [];
 const bag = JSON.parse(localStorage.getItem("prodsBag")) || [];
 render();
 
-const card1 = new products ( 1, "Colorblock Drop-Sleeve", 3550,"../Assets/Multimedia/fotos_women/Colorblock Drop-Sleeve Shirt 1.webp");
-const card2 = new products ( 2, "Hot Wheels Graphic Cami", 2000,"../Assets/Multimedia/fotos_women/Hot Wheels Graphic Cami 1.jpg");
-const card3 = new products ( 3, "Pointelle Knit Tank Top", 1500,"../Assets/Multimedia/fotos_women/Pointelle Knit Tank Top 1.webp");
-const card4 = new products ( 4, "Patchwork Bandana Crop Top", 2000,"../Assets/Multimedia/fotos_women/Patchwork Bandana Crop Top 1.jpg" );
+const card1 = new products(1, "Colorblock Drop-Sleeve", 3550, "../Assets/Multimedia/fotos_women/Colorblock Drop-Sleeve Shirt 1.webp");
+const card2 = new products(2, "Hot Wheels Graphic Cami", 2000, "../Assets/Multimedia/fotos_women/Hot Wheels Graphic Cami 1.jpg");
+const card3 = new products(3, "Pointelle Knit Tank Top", 1500, "../Assets/Multimedia/fotos_women/Pointelle Knit Tank Top 1.webp");
+const card4 = new products(4, "Patchwork Bandana Crop Top", 2000, "../Assets/Multimedia/fotos_women/Patchwork Bandana Crop Top 1.jpg");
 prods.push(card1, card2, card3, card4);
 
 for (const item of prods) {
@@ -28,20 +28,20 @@ for (const item of prods) {
     let btnAddToBag = document.getElementById(`${item.id}`).addEventListener("click", () => bagButton(item));
 }
 
-function render () {
-    const callModal = document.querySelector("#modalB");
+function render() {
+    const callModal = document.querySelector(".cartContainer");
     bag.forEach(item => {
         let bagItemContent = document.createElement("div");
+        bagItemContent.classList = "prod-container";
         bagItemContent.innerHTML = `
-        <p>Name:${item.name} Price: $${item.price}</p>  
+        <p> Name:${item.name} Price: $${item.price}</p>  
         `
+        callModal.append(bagItemContent);
     })
-    callModal.append(bagItemContent);  
 }
 
-function bagButton (item) {
+function bagButton(item) {
     bag.push(item);
     render();
     localStorage.setItem("prodsBag", JSON.stringify(item));
 }
-
